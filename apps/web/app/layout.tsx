23f201955fdece95f19bcb0bfae8ee18d1a2d4e7
Chung-il Jung <jungchungil7@gmail.com>
2024-05-25 20:25:01 +0900

    초기세팅 react19, next15

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "funfact",
+  description:
+    "simple and enjoyable app that delivers random and personalized fun facts",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@funfact/ui/button";
+
+export default function Page() {
+  return (
+    <main>
+      <Button appName="web">Click me!</Button>
+    </main>
+  );
+}
